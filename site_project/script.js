7d3e31c9ba8b179b14848214908a7c7d4045caf4
DzeroIgor <123312659+DzeroIgor@users.noreply.github.com>
2023-07-18 22:13:32 +0300

    Update script.js

--- site_project/script.js
+++ site_project/script.js
@@ -1,11 +1,20 @@
-$('.dropdown').hover(
-  function() {
-    $(this).addClass('show');
-    $(this).find('.dropdown-menu-main').addClass('show');
-  },
-  function() {
-    $(this).removeClass('show');
-    $(this).find('.dropdown-menu-main').removeClass('show');
-  }
-);
+var dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(function(dropdown) {
+  dropdown.addEventListener('mouseenter', function() {
+    this.classList.add('show');
+    var dropdownMenu = this.querySelector('.dropdown-menu-main');
+    if (dropdownMenu) {
+      dropdownMenu.classList.add('show');
+    }
+  });
+
+  dropdown.addEventListener('mouseleave', function() {
+    this.classList.remove('show');
+    var dropdownMenu = this.querySelector('.dropdown-menu-main');
+    if (dropdownMenu) {
+      dropdownMenu.classList.remove('show');
+    }
+  });
+});
 
